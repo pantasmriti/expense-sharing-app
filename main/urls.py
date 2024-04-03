9bf858eb85e0a6283b593a119a41539baa65bc73
Smriti <pantasmriti2002@gmail.com>
2024-04-03 15:57:38 +0545

    pull

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from accounts.views import register,user_login,user_logout
 from main import views
 urlpatterns = [
-    path('accounts/login/', user_login ,name="login"),
+    path('', user_login ,name="login"),
     path('register', register ,name="register"),
     path('logout', user_logout,name="logout"),
-    path('',views.home,name="homepage"),
+    path('home',views.home,name="homepage"),
     path('category/list', views.expense_category_list, name='expense_category_list'),
     path('category/create/', views.expense_category_create, name='expense_category_create'),
     path('category/update/<int:pk>/', views.expense_category_update, name='expense_category_update'),
